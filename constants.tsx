9ab09b30055b7044dc37988d1bb2d07cbeadaeea
Surya Dixit <surya@example.com>
2026-02-03 00:21:59 +0530

    Update constants

--- constants.tsx
+++ constants.tsx
@@ -1,4 +1,3 @@
-
 import { NavLink, Service, Industry } from './types';
 
 export const NAV_LINKS: NavLink[] = [
@@ -11,6 +10,7 @@ export const NAV_LINKS: NavLink[] = [
       'CCTV SYSTEM',
       'Access Control System',
       'Emergency Door Release Or Green MCP',
+      'Fire Exit Doors',
       'Fire Alarm System',
       'Fire Extinguishers',
       'Intruder Alarm System',
@@ -35,6 +35,7 @@ export const SERVICES: Service[] = [
   { id: '7', title: 'Water Leak Detection', image: 'https://www.gdcreations.in/assets/img/s8.jpg' },
   { id: '8', title: 'EPABX', image: 'https://www.gdcreations.in/assets/img/s10.png' },
   { id: '9', title: 'Networking', image: 'https://images.unsplash.com/photo-1544197150-b99a580bb7a8?auto=format&fit=crop&q=80&w=800' },
+  { id: '10', title: 'Fire Exit Doors', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE0WnDGZlnRrwpz5oVn_x7yHdzMC8Ed1l7Ww&s' },
 ];
 
 export const INDUSTRIES: Industry[] = [
